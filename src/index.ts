dd4032f7dc120dbc16c02108ac72d341503d2a64
tatsuya <t2h5k0@gmail.com>
2020-01-23 17:00:22 +0900

    add index.ts for express

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+import * as bodyParser from "body-parser";
+import express from "express";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post(`/users`, async (req: Request, res: Response) => {
+  const result = await prisma.users.create({
+    data: {
+      ...req.body,
+    }
+  });
+  res.json(result);
+});
+
+
+app.get(`/users/:id`, async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const post = await prisma.users.findOne({
+    where: {
+      user_id: Number(id),
+    }
+  });
+  res.json(post);
+});
+
+const server = app.listen(3000, () =>
+  console.log("server ready at: http:localhost:3000")
+);
